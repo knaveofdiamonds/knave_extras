b009d166da3a737283c0db710c9594b8a272e8c8
Roland Swingler <roland.swingler@gmail.com>
2009-07-26 15:08:48 +0100

    Minor tweaks

--- rails_generators/model/templates/model.rb
+++ rails_generators/model/templates/model.rb
@@ -1,7 +1,9 @@
 class <%= class_name %> < ActiveRecord::Base
+<% unless options[:skip_timestamps] -%>
   attr_protected :updated_at, :created_at
   attr_readonly  :created_at
 
+<% end -%>
   # Relationships
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %>
@@ -9,7 +11,7 @@ class <%= class_name %> < ActiveRecord::Base
 
   # Validations
   validates_presence_of <%= attributes.map {|a| ":" + a.name }.join(", ") %>
-<% unless numeric_attributes.empty? %>
+<% unless numeric_attributes.empty? -%>
   validates_numericality_of <%= numeric_attributes.map {|a| ":" + a.name }.join(", ") %>
 <% end -%>
 end
