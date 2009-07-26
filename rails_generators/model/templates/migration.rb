0d15804c1b356b24067f5161b5cc835dffedbdbc
Roland Swingler <roland.swingler@gmail.com>
2009-07-26 14:20:50 +0100

    adding model generators

---
+++ rails_generators/model/templates/migration.rb
@@ -0,0 +1,20 @@
+class <%= migration_name %> < ActiveRecord::Migration
+  def self.up
+    create_table :<%= table_name %> do |t|
+<% for attribute in attributes -%>
+      t.<%= attribute.type %> :<%= attribute.name %>, :null => false
+<% end -%>
+<% unless options[:skip_timestamps] %>
+      t.timestamps
+<% end -%>
+    end
+
+<% attributes.select(&:reference?).each do |attribute| -%>
+    add_index :<%= table_name %>, :<%= attribute_name %>
+<% end -%>
+  end
+
+  def self.down
+    drop_table :<%= table_name %>
+  end
+end
---
+++ rails_generators/model/templates/model.rb
@@ -0,0 +1,15 @@
+class <%= class_name %> < ActiveRecord::Base
+  attr_protected :updated_at, :created_at
+  attr_readonly  :created_at
+
+  # Relationships
+<% attributes.select(&:reference?).each do |attribute| -%>
+  belongs_to :<%= attribute.name %>
+<% end -%>
+
+  # Validations
+  validates_presence_of <%= attributes.map {|a| ":" + a.name }.join(", ") %>
+<% unless numeric_attributes.empty? %>
+  validates_numericality_of <%= numeric_attributes.map {|a| ":" + a.name }.join(", ") %>
+<% end -%>
+end
