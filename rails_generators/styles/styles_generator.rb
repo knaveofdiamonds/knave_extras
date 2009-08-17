9b55d26d2f70622f55ab650fe468be098379a04c
Roland Swingler <roland.swingler@gmail.com>
2009-08-17 22:50:34 +0100

    Fixing bug with resets.css

--- rails_generators/styles/styles_generator.rb
+++ rails_generators/styles/styles_generator.rb
@@ -3,7 +3,7 @@ class StylesGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.directory('public/stylesheets')
-      m.file "reset.css", "public/stylesheets/styles.css"
+      m.file "resets.css", "public/stylesheets/resets.css"
       m.file "typography.less", "public/stylesheets/typography.less"
     end
   end
